08438af835e391a8d3cfeac43f927618eb7f8953
Dzmitry Mikhalchuk <dmitry.mikhalchuk@gmail.com>
2026-06-08 15:45:30 +0200

    ci: add allure trends

--- allure.config.mjs
+++ allure.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "allure";
 
 export default defineConfig({
+  historyPath: "./allure-history",
   qualityGate: {
     rules: [{ successRate: 1.0, maxFailures: 0 }],
   },
