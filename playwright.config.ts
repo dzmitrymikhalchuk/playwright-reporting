d1da72a6236a4720dfb2b430227a392528b9664e
Dzmitry Mikhalchuk <dmitry.mikhalchuk@gmail.com>
2026-06-08 15:07:11 +0200

    ci: reporting minor fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["line"], ["html", { open: "never" }], ["junit", { outputFile: "test-results/results.xml" }], ['allure-playwright', {
+  reporter: [["line"], ["html", { open: "never", theme: "system" }], ["junit", { outputFile: "test-results/results.xml" }], ['allure-playwright', {
       detail: false,
     }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
